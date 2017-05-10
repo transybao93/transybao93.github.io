5a7cf6f34dd8a65634fc584a8a70af9ff9d7aeb5
transybao93 <IT_coder_93@yahoo.com.vn>
2017-05-10 16:49:57 +0700

    Update elisa translate

--- js/elisa.js
+++ js/elisa.js
@@ -1,35 +1,34 @@
 $(document).ready(function () {
 
-    $('#btnTranslate').bind('click', function()
-    {
-        var text = $('#dataVi').val();
-        var from = "vi";
-        var to = "ja";
-        translate(text, from, to);
-    });
-
-    $(document).keypress(function(event) {
-        var keycode = event.keyCode || event.which;
-        if(keycode == '13') {
-            var text = $('#dataVi').val();
-            var from = "vi";
-            var to = "ja";
-            translate(text, from, to);
-        }
-    });
-    // $.get("http://www.transltr.org/api/translate?text=xin%20ch%C3%A0o&to=ja&from=vi", function(data, status){
-    //     alert("Data: " + data.translationText + "\nStatus: " + status);
+    // $('#btnTranslate').bind('click', function()
+    // {
+    //     var text = $('#dataVi').val();
+    //     var from = "vi";
+    //     var to = "ja";
+    //     translate(text, from, to);
     // });
 
-    function translate(text, from, to)
-    {
-        $.get("http://www.transltr.org/api/translate?text=" + text + "&to=" + to + "&from=" + from, function(data, status){
-            if(status == "success")
-            {
-                $('.translated').text(data.translationText);
-            }
-        });
-    }
+    // $(document).keypress(function(event) {
+    //     var keycode = event.keyCode || event.which;
+    //     if(keycode == '13') {
+    //         var text = $('#dataVi').val();
+    //         var from = "vi";
+    //         var to = "ja";
+    //         translate(text, from, to);
+    //     }
+    // });
+
+    // function translate(text, from, to)
+    // {
+    //     $.get("http://www.transltr.org/api/translate?text=" + text + "&to=" + to + "&from=" + from, function(data, status){
+    //         if(status == "success")
+    //         {
+    //             $('.translated').text(data.translationText);
+    //         }
+    //     });
+    // }
+
+    alert('Xin chào');
 
 });
 
