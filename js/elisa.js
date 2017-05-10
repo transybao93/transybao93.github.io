9d3bc7d2de1445fec445af47916114f50c0f001e
transybao93 <IT_coder_93@yahoo.com.vn>
2017-05-10 17:38:19 +0700

    Update elisa translate

--- js/elisa.js
+++ js/elisa.js
@@ -1,34 +1,31 @@
 $(document).ready(function () {
 
-    // $('#btnTranslate').bind('click', function()
-    // {
-    //     var text = $('#dataVi').val();
-    //     var from = "vi";
-    //     var to = "ja";
-    //     translate(text, from, to);
-    // });
+    $('#btnTranslate').bind('click', function()
+    {
+        var text = $('#dataVi').val();
+        var from = "vi";
+        var to = "ja";
+        translate(text, from, to);
+    });
 
-    // $(document).keypress(function(event) {
-    //     var keycode = event.keyCode || event.which;
-    //     if(keycode == '13') {
-    //         var text = $('#dataVi').val();
-    //         var from = "vi";
-    //         var to = "ja";
-    //         translate(text, from, to);
-    //     }
-    // });
+    $(document).keypress(function(event) {
+        var keycode = event.keyCode || event.which;
+        if(keycode == '13') {
+            var text = $('#dataVi').val();
+            var from = "vi";
+            var to = "ja";
+            translate(text, from, to);
+        }
+    });
 
-    // function translate(text, from, to)
-    // {
-    //     $.get("http://www.transltr.org/api/translate?text=" + text + "&to=" + to + "&from=" + from, function(data, status){
-    //         if(status == "success")
-    //         {
-    //             $('.translated').text(data.translationText);
-    //         }
-    //     });
-    // }
-
-    alert('Xin chào');
+    function translate(text, from, to)
+    {
+        $.get("http://www.transltr.org/api/translate?text=" + text + "&to=" + to + "&from=" + from, function(data, status){
+            if(status == "success")
+            {
+                $('.translated').text(data.translationText);
+            }
+        });
+    }
 
 });
-
